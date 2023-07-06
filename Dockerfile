c8d8082ab7bcdda9aaa26293b8e045304bdf2f9e
Lucas Moreira <lucas-mopi@hotmail.com>
2023-07-05 21:39:37 -0300

    feat: implements the jplag plagiarism detector and add TaskServiceLogs in run plagiarism detector task

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,25 @@
-FROM maven:3.8.6-eclipse-temurin-17
+FROM python:3.9
 
-# clone specific version
-RUN mkdir /jplag && \
-  cd /jplag && \
-  git clone https://github.com/jplag/JPlag.git . && \
-  git checkout b886f5504e1f86df331273aaa425dbf672268d48
+# Set up working directory
+WORKDIR /jplag
 
-# build jplag
-RUN cd /jplag && \
-  mvn clean package assembly:single
+# Install Maven and other required packages
+RUN apt-get update && apt-get install -y maven wget
 
-RUN ls -l /jplag/jplag.cli/target
+# Download JPlag
+RUN wget https://github.com/jplag/JPlag/releases/download/v4.3.0/jplag-4.3.0-jar-with-dependencies.jar
+
+# Install Flask and Gunicorn
+RUN pip3 install flask==2.0.3 gunicorn==20.1.0
+
+# Copy the source code
+COPY app.py .
+# We can remove this lines. It's only dummy code to make some tests
+COPY code1.cpp .
+COPY code2.cpp .
+
+# Expose the container port
+EXPOSE 5000
+
+# Start the application using Gunicorn
+CMD ["gunicorn", "-b", "0.0.0.0:5000", "app:app"]
---
+++ app.py
@@ -0,0 +1,86 @@
+import os
+import json
+import zipfile
+import base64
+import tempfile
+from subprocess import check_output, CalledProcessError
+from flask import Flask, Response, request
+from logging.config import dictConfig
+import sys
+sys.stdout.flush()
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+JPLAG_FILE = "jplag-4.3.0-jar-with-dependencies.jar"
+app = Flask(__name__)
+
+def extract_result(zip_file_path):
+    with zipfile.ZipFile(f"{zip_file_path}.zip", "r") as zip_ref:
+        # Extract the JSON files from the zip file
+        json_files = [file for file in zip_ref.namelist() if file.endswith(".json")]
+        results = []
+
+        # Process each JSON file
+        for json_file in json_files:
+            # Read the JSON content
+            with zip_ref.open(json_file) as file:
+                json_data = file.read()
+
+            # Parse the JSON data
+            data = json.loads(json_data)
+            results.append(data)
+
+        return results
+
+@app.route('/', methods=['POST'])
+def detect_plagiarism():
+    extension = request.json.get('extension')
+    submissions = request.json.get('submissions', [])
+    response = {"errors": []}
+
+    # Create a temporary directory to store the submission
+    # temp_dir = tempfile.mkdtemp()
+    with tempfile.TemporaryDirectory() as temp_dir:
+        for submission in submissions:
+            # Save the submission to a temporary file
+            # submission_path: str = f"{temp_dir}/{submission['id']}.{extension}"
+            submission_path = os.path.join(temp_dir, f"code{submission['id']}.{extension}")
+            with open(submission_path, 'wb') as file:
+                # Decode the submission from base64
+                file.write(base64.b64decode(submission['code']))
+
+        results_file = os.path.join(temp_dir, "results")
+        command = ["java", "-jar", JPLAG_FILE, temp_dir, "-l", extension, "-r", results_file]
+        # java -jar jplag-4.3.0-jar-with-dependencies.jar /tmp/teste/ -l cpp -r /tmp/teste/r1/results
+        try:
+            output_response = check_output(command).decode('utf-8')
+            response["command_output"] = output_response
+        except CalledProcessError as error:
+            response["errors"].append(str(error.output))
+        except Exception as error:
+            response["errors"].append(str(error))
+        else:
+            response["results"] = extract_result(results_file)
+
+        return Response(
+            json.dumps(response),
+            status=200,
+            mimetype='application/json',
+        )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
