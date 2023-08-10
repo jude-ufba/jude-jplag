2d12216e8d42d2432e720caad08bdc727cae6489
Lucas Moreira <lucas-mopi@hotmail.com>
2023-08-10 09:09:59 -0300

    chore: add some dummy code to make tests

---
+++ code2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int factorial(int n) {
+    int result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+    std::cout << "Factorial of " << number << " is: " << factorial(number) << std::endl;
+    return 0;
+}
